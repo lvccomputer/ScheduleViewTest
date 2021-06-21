093535fc0cf3d3bec910be1abe6f6e58588de5f2
Le Van Cuong <levancuongit89@gmail.com>
2021-06-21 15:55:48 +0700

    init project

---
+++ app/src/main/java/com/test/testrecycler/MainActivity.java
@@ -0,0 +1,62 @@
+package com.test.testrecycler;
+
+import android.os.Bundle;
+import android.text.format.Time;
+import android.view.Menu;
+import android.widget.ViewSwitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.code.yadview.DayView;
+import com.google.code.yadview.EventResource;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EventResource mEventResource;
+    private YadviewHarnessDayViewFactory mViewFactory;
+
+    public MainActivity() {
+        mEventResource = new MockEventResource();
+//	    mEventResource = new DefaultEventResource(this, new DefaultUtilFactory("yadview_harness.prefs"));
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewSwitcher vs = (ViewSwitcher) findViewById(R.id.view_switcher);
+        mViewFactory = new YadviewHarnessDayViewFactory(vs, mEventResource, this);
+        vs.setFactory(mViewFactory);
+        DayView dv = (DayView) vs.getCurrentView();
+        Time today = new Time();
+        today.setToNow();
+        dv.setSelected(today, false, false);
+        dv.clearCachedEvents();
+        dv.reloadEvents();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mViewFactory.getEventLoader().startBackgroundThread();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mViewFactory.getEventLoader().stopBackgroundThread();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+}
